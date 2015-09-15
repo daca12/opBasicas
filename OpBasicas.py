2d9ff8794814bf5be117dd9610cb6813a1ae8188
daca12 <donovan_garga@hotmail.com>
2015-09-14 23:33:22 -0500

    Programa basico

---
+++ OpBasicas.py
@@ -0,0 +1,15 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> suma=12+2
+>>> resta=12-2
+>>> multiplicacion=12*2
+>>> division=12/2
+>>> print(suma)
+14
+>>> print(resta)
+10
+>>> print(multiplicacion)
+24
+>>> print(division)
+6.0
+>>> 
